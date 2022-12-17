7d95c055bbdd2fa7f9a5533c098c0aee8ee7d611
BonacePatrick <patrickruboneza@gmail.com>
2022-12-17 14:20:02 +0200

    add responsive to home

--- src/components/Home/NavHero.jsx
+++ src/components/Home/NavHero.jsx
@@ -10,12 +10,12 @@ const NavHero = () => {
             <label tabIndex={0} className="btn btn-ghost btn-circle text-white bg-[#333333]">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3  shadow bg-[#333333] rounded-box w-14 h-72 relative text-white">
-                <li className='ml-6 hover:text-[#daa320]'><i class="fa-solid fa-house lg:-translate-x-4 lg:translate-y-8 hover:text-[#daa320]"></i><Link to='/' className='translate-x-5'>HOME</Link></li>
-                <li className='ml-6 hover:text-[#daa320]'><i class="fa-solid fa-user lg:-translate-x-4 lg:translate-y-1 my-3 hover:text-[#daa320]"></i><Link to='/About' className='-translate-y-10 translate-x-5'>ABOUT</Link></li>
-                <li className='ml-6 hover:text-[#daa320]'><i class="fa-solid fa-briefcase lg:-translate-x-4 lg:-translate-y-5 mb-3 hover:text-[#daa320]"></i><Link to='/Portfolio' className='-translate-y-16 translate-x-5'>PORTFOLIO</Link></li>
-                <li className='ml-6 hover:text-[#daa320] absolute top-56'><i class="fa-solid fa-comments lg:-translate-x-4 my-2 lg:-translate-y-12 hover:text-[#daa320]"></i><Link to='/Contact' className='translate-x-5 -translate-y-[5.5rem]'>CONTACT</Link></li>
-                <li className='ml-6 hover:text-[#daa320] absolute top-72'><i class="fa-solid fa-envelope lg:-translate-x-4 lg:-translate-y-16 hover:text-[#daa320]"></i><Link to='/Blog' className='translate-x-5 -translate-y-24'>BLOG</Link></li>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3  shadow bg-[#333333] rounded-box w-14 h- lg:h-72 relative text-white">
+                <li className='ml-6 hover:text-[#daa320]'><i class="fa-solid fa-house lg:-translate-x-4 lg:translate-y-8 hover:text-[#daa320]  -translate-x-5 translate-y-3 text-xs lg:text-sm"></i><Link to='/' className='translate-x-5 -translate-y-4 lg:translate-y-0 text-xs lg:text-sm'>HOME</Link></li>
+                <li className='ml-6 hover:text-[#daa320]'><i class="fa-solid fa-user lg:-translate-x-4 lg:translate-y-1 lg:my-3 hover:text-[#daa320] -translate-x-5 -translate-y-4 text-xs lg:text-sm"></i><Link to='/About' className='-translate-y-[2.8rem] lg:-translate-y-10 translate-x-5 text-xs lg:text-sm'>ABOUT</Link></li>
+                <li className='ml-6 hover:text-[#daa320]'><i class="fa-solid fa-briefcase lg:-translate-x-4 lg:-translate-y-5 lg:mb-3 hover:text-[#daa320] -translate-x-5 -translate-y-12 text-xs lg:text-sm"></i><Link to='/Portfolio' className='-translate-y-[4.8rem] lg:-translate-y-16 translate-x-5 text-xs lg:text-sm'>PORTFOLIO</Link></li>
+                <li className='ml-6 hover:text-[#daa320] absolute top-36 lg:top-56'><i class="fa-solid fa-comments lg:-translate-x-4 my-2 lg:-translate-y-12 hover:text-[#daa320] -translate-y-12  -translate-x-5 text-xs lg:text-sm"></i><Link to='/Contact' className='translate-x-5 -translate-y-[5.5rem] text-xs lg:text-sm'>CONTACT</Link></li>
+                <li className='ml-6 hover:text-[#daa320] absolute top-[8.5rem] lg:top-72'><i class="fa-solid fa-envelope lg:-translate-x-4 lg:-translate-y-16 hover:text-[#daa320]  -translate-x-5 text-xs lg:text-sm"></i><Link to='/Blog' className='lg:translate-x-5 lg:-translate-y-24 -translate-y-6 translate-x-5 text-xs lg:text-sm'>BLOG</Link></li>
             </ul>
             </div>
         </div>
@@ -27,7 +27,7 @@ const NavHero = () => {
         <img className='w-full' src="./assets/profile.png" alt="" />
       </div>
 
-      <div className="flex flex-col justify-center lg:mr-20 mt-32 lg:mt-0 ">
+      <div className="flex flex-col justify-center lg:mr-20 mt-32 lg:mt-0 ml-5 lg:ml-0 ">
       <div className="greet text-white">
         <h1>HI THERE!</h1>
       </div>
