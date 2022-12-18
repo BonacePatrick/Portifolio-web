a09ff9adb9bba71f23923ae0eb58bb73e819a2cf
BonacePatrick <patrickruboneza@gmail.com>
2022-12-18 13:52:53 +0200

    add some

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", "./index.html",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.jsx',
-    'node_modules/flowbite/**/*.jsx',
-    'node_modules/flowbite-react/**/*.js',
-    'node_modules/flowbite/**/*.js',
     './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
