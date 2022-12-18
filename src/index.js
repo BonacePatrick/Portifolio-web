69aedc2d0e0ceafc4a24ccb57ce6f3ab7b9978fc
BonacePatrick <patrickruboneza@gmail.com>
2022-12-18 14:06:58 +0200

    some

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tw-elements';
-import 'flowbite';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", "./index.html",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
     './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
@@ -10,7 +9,6 @@ module.exports = {
   },
   plugins: [
     require("daisyui"),
-    require('flowbite/plugin'),
     require('tw-elements/dist/plugin')
   ],
 }
